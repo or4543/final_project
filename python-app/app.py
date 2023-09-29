2cdd00a34f54800510901e16e481e4b6e9b2d07c
or4543 <ordahaman2@gmail.com>
2023-09-29 12:38:38 +0300

    my final project

---
+++ python-app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB connection setup
+client = MongoClient('mongodb://root:3yGWpZ7jeS@34.78.116.136:27017/')  # Replace with your MongoDB connection URI
+db = client['Or_dk']  # Replace with your database name
+collection = db['links']  # Replace with your collection name
+
+@app.template_filter('zip_lists')
+def zip_lists(a, b):
+    return zip(a, b)
+
+@app.route('/')
+def index():
+
+   # Retrieve links from MongoDB
+    links = collection.find_one()['links']
+    
+    # Define labels for the links
+    labels = ['LinkedIn', 'GitHub', 'GitLab']
+
+    # Render the HTML template and pass the links as variables
+    return render_template('index.html', links=links, labels=labels)
+
+if __name__ == '__main__':
+     app.run(host="0.0.0.0",port=5000,debug=True)
---
+++ python-app/test.py
@@ -0,0 +1,8 @@
+import requests
+
+def test_app_health_check():
+    response = requests.get("http://localhost:5000")
+    assert response.status_code == 200
+    assert "app" in response.text
+
+# Add more test cases as needed
